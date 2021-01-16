f81b497567aad1fd656afafd57dee06902f0905c
Mario Lozano <mario@outcrop.com>
2021-01-16 15:52:50 -0700

    Adding functionality to custom module

--- web/modules/custom/mymodule/src/Controller/MymoduleController.php
+++ web/modules/custom/mymodule/src/Controller/MymoduleController.php
@@ -2,7 +2,13 @@
 
 namespace Drupal\mymodule\Controller;
 
+use Drupal\Component\Serialization\Json;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Http\ClientFactory;
+use Drupal\Core\Render\RendererInterface;
+use GuzzleHttp\Client;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
  * Returns responses for mymodule routes.
@@ -10,13 +16,46 @@ use Drupal\Core\Controller\ControllerBase;
 class MymoduleController extends ControllerBase {
 
   /**
+   * @var RendererInterface
+   */
+  private $renderer;
+  /**
+   * @var ClientFactory
+   */
+  private $httpClient;
+
+  public function __construct(RendererInterface $renderer, ClientFactory $http_client) {
+    $this->renderer = $renderer;
+    $this->httpClient = $http_client;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('renderer'),
+      $container->get('http_client_factory')
+    );
+  }
+
+  /**
    * Builds the response.
    */
-  public function build() {
+  public function teamList() {
+
+    $client = $this->httpClient->fromOptions([
+      'base_uri' => 'http://delivery.chalk247.com',
+    ]);
+
+    $response = $client->get('/team_list/NFL.JSON', [
+      'query' => [
+        'api_key' => '74db8efa2a6db279393b433d97c2bc843f8e32b0',
+      ],
+    ]);
+
+    $result = Json::decode($response->getBody());
 
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+      '#markup' => $this->t('Here I should display the team list!'),
     ];
 
     return $build;
